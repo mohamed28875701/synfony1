7dabd4382ec74ac59e49f7149e2cef22f1deaf68
mohamed28875701 <mohamedbenhadjlakhal@gmail.com>
2023-10-11 19:10:23 +0100

    Atelier 4 partie 1

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -1,20 +1,22 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Author;
+use App\Form\AuthorType;
+use App\Repository\AuthorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+
 
 class AuthorController extends AbstractController
 {
     public $authors = array(
-        array('id' => 1, 'picture' => '/images/Victor-Hugo.jpg','username' => 'Victor Hugo', 'email' =>
-            'victor.hugo@gmail.com ', 'nb_books' => 100),
-        array('id' => 2, 'picture' => '/images/william-shakespeare.jpg','username' => ' William Shakespeare', 'email' =>
-            ' william.shakespeare@gmail.com', 'nb_books' => 200 ),
-        array('id' => 3, 'picture' => '/images/Taha_Hussein.jpg','username' => 'Taha Hussein', 'email' =>
-            'taha.hussein@gmail.com', 'nb_books' => 300),
+        
     );
     #[Route('/author', name: 'app_author')]
     public function index(): Response
@@ -28,9 +30,10 @@ class AuthorController extends AbstractController
         return $this->render('author/show.html.twig', ["name" => $name]);
     }
     #[Route('/author/{name}', name: 'list')]
-    public function list(){
-
-        return $this->render('author/list.html.twig',["authors"=>$this->authors]);
+    public function list(AuthorRepository $repository){
+        
+        
+        return $this->render('author/list.html.twig',["authors"=>$repository->findAll()]);  
     }
     #[Route('/authorDetails/{id}',name:'authorDetails')]
     public function authorDetails($id){
@@ -41,5 +44,47 @@ class AuthorController extends AbstractController
             }
         }
         return $this->render('author/authorDetails.html.twig',["author"=>$author]);
+    }
+    #[Route('/affiche',name:'auth_affiche')]
+    public function affiche(AuthorRepository $repo){
+        $author=$repo->findAll();
+        return $this->render('author/affiche.html.twig',['author'=>$author]);
+    }
+    #[Route('/add',name:'add_author')]
+    public function add_author(ManagerRegistry $reg,Request $request){
+        $author=new Author();
+        $form =$this->CreateForm(AuthorType::class,$author);
+        $form->add('add',SubmitType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $em=$reg->getManager();
+            $em->persist($author);
+            $em->flush();
+            return $this->redirectToRoute('auth_affiche');
+        }
+        return $this->render('author/Add.html.twig',['f'=>$form->createView()]);
+    }
+    #[Route('/edit/{id}',name:'auth_edit')]
+    public function editAuthor($id,AuthorRepository $repo,ManagerRegistry $registry,Request $req){
+        $author=$repo->find($id);
+        $form=$this->createForm(AuthorType::class,$author);
+        $form->add('edit',SubmitType::class);
+        $form->handleRequest($req);
+        if($form->isSubmitted() && $form->isValid()){
+            $em=$registry->getManager();
+            $em->flush();
+            return $this->redirectToRoute("auth_affiche");
+        }
+        return $this->render("author/edit.html.twig",["f"=>$form->createView()]);
+    }
+    #[Route('/delete/{id}',name:'auth_delete')]
+    public function deleteAuthor($id,AuthorRepository $repo,ManagerRegistry $registry){
+        $author=$repo->find($id);
+        if(!$author) return;
+        $em = $registry->getManager();
+        $em->remove($author);
+        $em->flush();
+        return $this->redirectToRoute("auth_affiche");
     }
 }
